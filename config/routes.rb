b10f1b519f128515fe2080cb36b8b453ce0cbbfc
Mark Varney <mvarney@uncc.edu>
2018-11-05 22:32:06 +0000

    Updated main controller, action, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'main/list'
   get 'welcome/index'
-  
+  get 'articles/new'
+  resources :articles
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
