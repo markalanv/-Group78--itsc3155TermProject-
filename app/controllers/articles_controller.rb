39069668366295e2e55cd99560a3711d01e16d21
Majd Fattah <mfattah@uncc.edu>
2018-11-14 22:47:31 +0000

    Add Edit/Update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,18 @@ class ArticlesController < ApplicationController
     else 
       render 'new'
     end
-
+  def edit 
+      @article = Article.find(params[:id]) 
+  end
+  def update
+      @article = Article.find(params[:id])
+      
+      if @article.update(article_params)
+        redirect_to @article
+  else
+      render 'edit'
+      end
+  end
   end
   def show
     @article = Article.find(params[:id])
