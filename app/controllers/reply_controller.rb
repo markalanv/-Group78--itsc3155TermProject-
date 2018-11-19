05f8eb1f4ac589363ba86268522b0b835c5a59f5
Majd Fattah <mfattah@uncc.edu>
2018-11-19 00:29:14 +0000

    Add reply controller

---
+++ app/controllers/reply_controller.rb
@@ -0,0 +1,12 @@
+class ReplyController < ApplicationController
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+ 
+    private
+        def comment_params
+            params.require(:comment).permit(:name, :date, :reply)
+        end
+end
