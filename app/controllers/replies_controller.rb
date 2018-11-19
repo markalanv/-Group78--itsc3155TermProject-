0dc6d78b5ef1822d7b05c020fa0476864bdc9e49
Majd Fattah <mfattah@uncc.edu>
2018-11-19 03:12:02 +0000

    add a partial to display replies and destroy functionality for replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,7 +4,12 @@ class RepliesController < ApplicationController
         @reply = @article.replies.create(reply_params)
         redirect_to article_path(@article)
      end
-    
+    def destroy
+        @article = Article.find(params[:article_id])
+        @reply = @article.replies.find(params[:id])
+        @reply.destroy
+        redirect_to article_path(@article)
+    end
     private
         def reply_params
             params.require(:reply).permit(:name, :date, :reply)
