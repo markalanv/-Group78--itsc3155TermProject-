33dcbd2def59bbd93a37a187f981a305578e8a76
Your Name <you@example.com>
2018-12-04 18:40:54 +0000

    Added Cucumber rules

---
+++ spec/user_adds_a_blog_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper.rb'
+
+feature"Blogger adds new post" do
+    scenario "Blogger successfully navigates to new blog post page" do
+       visit articles_path
+       click_link "New Blog"
+       expect(page).to have_content("New 49er Blog")
+       expect(page).to have_field("Title")
+       expect(page).to have_field("Text")
+    end
+    
+    scenario "Blogger successfully adds new blog post" do
+       visit articles_path
+       click_link "New Blog"
+       fill_in "Title", with: "New Blog Post"
+       fill_in "Text", with: "This is a new blog post"
+       click_button "Create Article"
+       expect(page).to have_content("New Blog Post")
+       expect(page).to have_content("This is a new blog post")
+    end
+end
